93d18c772efda7fbbfb749874fcf03b283c10bc5
Emrehan Baysal <81330668+bymrhan@users.noreply.github.com>
2022-09-19 19:30:57 +0300

    1.Haftanın Ödevleri

---
+++ 1.Haftanın Ödevleri/dataTypes.java
@@ -0,0 +1,15 @@
+public class dataTypes {
+    public static void main(String[] args) {
+        double sayi = 12.5;
+        sayi = -129;
+
+        char karakter = 'A';
+        boolean dogruMu = false;
+
+        System.out.println(sayi);
+        System.out.println(karakter);
+        System.out.println(dogruMu);
+
+        /*Temel Veri Tipleriyle Çalışmak*/
+    }
+}
