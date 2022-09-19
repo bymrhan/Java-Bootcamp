8bb5e140874559673c68836a126fd7ce830acd93
Emrehan Baysal <81330668+bymrhan@users.noreply.github.com>
2022-09-19 19:34:47 +0300

    1.Haftanın Ödevleri

---
+++ 1.Haftanın Ödevleri/conditionals.java
@@ -0,0 +1,21 @@
+public class conditionals {
+    public static void main(String[] args) {
+        int sayı=24;
+
+        if(sayı<20) {
+            System.out.println("Sayı 20'den küçüktür!");
+        } else if(sayı==20){
+            System.out.println("Sayı 20'den küçük değildir!");
+        }
+        else {
+            System.out.println("Sayı 20'den büyüktür!");
+        }
+
+        /*Java'da mantıksal ve koşullu ifadeler için if blokları kullanılır.
+        Java 'da if blokları türkçede "eğer" cümlesinin verdiği anlamla aynı şeyi ifade etmektedir.
+        Programlamada koşul oluşturmak için if ve else bloklarını kullanırız.
+        Else deyimi "değil ise" anlamı katmaktadır ve if deyiminden sonra kullanılır.
+        Programlama da if ve else deyimleri çok önemlidir ve kodlamanın temel yapı taşlarını oluştururlar.
+        If ve Else blokları ile programımız içerisinde ki algoritmalarmızı oluştururuz.*/
+    }
+}
