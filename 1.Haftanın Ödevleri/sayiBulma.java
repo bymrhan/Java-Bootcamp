dff6b919ae7b0752ff985cbdadab840851b45329
Emrehan Baysal <81330668+bymrhan@users.noreply.github.com>
2022-09-20 00:07:26 +0300

    1.Haftanın Ödevleri

---
+++ 1.Haftanın Ödevleri/sayiBulma.java
@@ -0,0 +1,21 @@
+public class sayiBulma {
+    public static void main(String[] args) {
+        int[] sayilar = new int[]{1, 2, 3, 4, 5};
+        int aranacak = 5;
+        boolean varMi = false;
+
+        for (int sayi : sayilar) {
+            if (sayi == aranacak) {
+                varMi = true;
+                break;
+            }
+        }
+
+        if (varMi) {
+            System.out.println("Sayi mevcuttur");
+        } else {
+            System.out.println("Sayi mevcut degildir.");
+        }
+
+    }
+}
