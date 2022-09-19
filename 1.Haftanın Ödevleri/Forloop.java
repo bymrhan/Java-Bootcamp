69d085e9fcc1b52b76322e47bcd4c4dfee5563a7
Emrehan Baysal <81330668+bymrhan@users.noreply.github.com>
2022-09-19 20:55:46 +0300

    1.Haftanın Ödevleri

--- 1.Haftanın Ödevleri/Forloop.java
+++ 1.Haftanın Ödevleri/Forloop.java
@@ -6,16 +6,22 @@ public class Forloop {
         }
         System.out.println("For Döngüsü Bitti.");
 
-        /*Tek Sayılar*/
-        for (int i = 1; i < 10; i+=2) {
+        /*While*/
+        int i = 1;
+        while (i < 10) {
             System.out.println(i);
+            i++;
         }
-        System.out.println("For Döngüsü Bitti.");
+        System.out.println("While Döngüsü Bitti");
 
-        /*Çift Sayılar*/
-        for (int i = 0; i < 10; i+=2) {
-            System.out.println(i);
-        }
-        System.out.println("For Döngüsü Bitti.");
+        /*Do- While Döngüsü*/
+        int j = 1;
+        do {
+
+            System.out.println(j);
+            j += 2;
+
+        } while (j < 10);
+        System.out.println("Do-While Döngüsü Bitti");
     }
 }
