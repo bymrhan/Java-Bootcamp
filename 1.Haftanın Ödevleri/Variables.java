b6423dbb6824f0a828d0cf1de11b256f740d7abd
Emrehan Baysal <81330668+bymrhan@users.noreply.github.com>
2022-09-19 19:27:39 +0300

    1.Haftanın Ödevleri

---
+++ 1.Haftanın Ödevleri/Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+    public static void main(String[] args) {
+        int ögrenciSayısı= 12;
+        String mesaj="Öğrenci Sayım";
+        System.out.println(mesaj +" "+ ögrenciSayısı);
+        System.out.println(mesaj +" "+ ögrenciSayısı);
+        System.out.println("Öğrenci Sayım" +" "+ ögrenciSayısı);
+
+        /*Değişkenler ve Değişken Kullanım İhtiyacı*/
+    }
+}
